ad3248c32681c12c4becec213036d7b0ddee8051
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-18 22:49:00 +0100

    Update main.js

--- miro/die/main.js
+++ miro/die/main.js
@@ -14,11 +14,10 @@ miro.onReady(() => {
     miro.initialize({
       extensionPoints: {
         bottomBar: {
-          title: 'New Die 3',
+          title: 'New Die',
           svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
           onClick: async () => {
-            await miro.board.widgets.create({type: 'sticker', text: 'num.toString()' });
-            miro.showNotification('Test Notification')
+            await miro.board.widgets.create({type: 'shape', text: 'Test' });
           }
         }
       }
