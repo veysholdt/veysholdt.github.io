be3dd02cf24c1eba4679edc94bbc12fd56728ce2
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-18 22:41:06 +0100

    Create Widget on click

--- miro/die/main.js
+++ miro/die/main.js
@@ -9,3 +9,18 @@ async function spawn()
     var num = get_randome();
     await miro.board.widgets.create({type: 'sticker', text: num.toString() });
 }
+
+miro.onReady(() => {
+    miro.initialize({
+      extensionPoints: {
+        bottomBar: {
+          title: 'New Die 3',
+          svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+          onClick: async () => {
+            var num = get_randome();
+            await miro.board.widgets.create({type: 'sticker', text: num.toString() });
+          }
+        }
+      }
+    })
+  })
