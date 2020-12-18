79274ffc809936f46efd3ab4f6b145b2e7e33cc5
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-18 23:21:02 +0100

    Update main.js

--- miro/die/main.js
+++ miro/die/main.js
@@ -4,21 +4,35 @@ function get_randome()
     return Math.floor(Math.random() * 6) + 1;   // random integer from 1 to 6 
 }
 
-async function spawn()
-{
-    var num = get_randome();
-    await miro.board.widgets.create({type: 'sticker', text: num.toString() });
+async function createWidget(canvasX, canvasY) {
+    const widget = (await miro.board.widgets.create({type: 'shape', x:canvasX || 0, y:canvasY || 0}))[0]
+    miro.board.viewport.zoomToObject(widget)
 }
 
+const options = {
+    draggableItemSelector: '#box',
+    onClick: async (targetElement) => {
+      createWidget()
+    },
+    getDraggableItemPreview: (targetElement) => { //drag-started
+      return {url: HOTSPOT_PREVIEW}
+    },
+    onDrop: (canvasX, canvasY) => {
+      createWidget(canvasX, canvasY)
+    }
+  }
+  
 miro.onReady(() => {
+    miro.board.ui.initDraggableItemsContainer(document.body, options)  
     miro.initialize({
       extensionPoints: {
         bottomBar: {
-          title: 'New Die',
+          title: 'New Dice',
           svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
           onClick: async () => {
             let sticker = (await miro.board.widgets.create({type:'sticker', text: 'Hello'}))[0]
             await miro.board.viewport.zoomToObject(sticker)
+            sticker.onClick()
           }
         }
       }
