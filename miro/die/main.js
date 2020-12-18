e503e60dece4664e1657534dbe957a6436714aee
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-18 19:45:07 +0100

    Update main.js

--- miro/die/main.js
+++ miro/die/main.js
@@ -3,10 +3,3 @@ function get_randome()
 {
     return Math.floor(Math.random() * 6) + 1;;   // random integer from 1 to 6 
 }
-
-// async function spawn()
-// {
-//     var num = get_randome();
-
-//     await miro.board.widgets.create({type: 'sticker', text: num.toString() })
-// }
