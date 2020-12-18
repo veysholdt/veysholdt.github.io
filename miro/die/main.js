b559b8e48a8f32eeaba1a998e67aa4d2389a5d26
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-18 18:59:56 +0100

    changed function call

--- miro/die/main.js
+++ miro/die/main.js
@@ -4,9 +4,9 @@ function get_randome()
     return Math.floor(Math.random() * 6) + 1;;   // random integer from 1 to 6 
 }
 
-async function spawn()
-{
-    var num = get_randome();
+// async function spawn()
+// {
+//     var num = get_randome();
 
-    await miro.board.widgets.create({type: 'sticker', text: num.toString() })
-}
+//     await miro.board.widgets.create({type: 'sticker', text: num.toString() })
+// }
