68d8cf4c05d62e1842cda63017671092647d1475
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-18 19:52:33 +0100

    fixed typos

--- miro/die/main.js
+++ miro/die/main.js
@@ -1,5 +1,5 @@
 
 function get_randome() 
 {
-    return Math.floor(Math.random() * 6) + 1;;   // random integer from 1 to 6 
+    return Math.floor(Math.random() * 6) + 1;   // random integer from 1 to 6 
 }
