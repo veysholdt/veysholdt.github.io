fd227d62b7d6e25b9a97df931d837640454b7ba7
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-19 12:26:39 +0100

    Longer generation

--- miro/die/main.js
+++ miro/die/main.js
@@ -21,7 +21,7 @@ miro.onReady(() => {
     miro.initialize({
       extensionPoints: {
         bottomBar: {
-          title: 'New Dice 5',
+          title: 'New Dice 6',
           svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
           onClick: async () => {
 
