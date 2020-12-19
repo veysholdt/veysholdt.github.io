1abd15cd251b28c41cffc7cf72d44ec6022fcfe5
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-19 12:14:01 +0100

    Update main.js

--- miro/die/main.js
+++ miro/die/main.js
@@ -4,8 +4,13 @@ function get_randome()
     return Math.floor(Math.random() * 6) + 1;   // random integer from 1 to 6 
 }
 
-async function createWidget(canvasX, canvasY) {
-    const widget = (await miro.board.widgets.create({type: 'shape', x:canvasX || 0, y:canvasY || 0}))[0]
+function get_random_color() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 function sleep(ms) {
@@ -16,25 +21,36 @@ miro.onReady(() => {
     miro.initialize({
       extensionPoints: {
         bottomBar: {
-          title: 'New Dice 3',
+          title: 'New Dice 4',
           svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
           onClick: async () => {
 
             var num = get_randome();
 
             let dice = (await miro.board.widgets.create(
-              { type:'shape', 
+              { type:'sticker', 
                 text: num.toString(),
                 x: 0.0,
                 y: 0.0,
                 capabilities: {
                 "editable": false
                 },
-                style: {
-                  backgroundColor: white,
-                  fontSize: 10,
-                }
             }))[0];
+
+            for (let i = 0; i < 10; i++) 
+            {
+              var num = get_randome();
+              var color = get_random_color();
+
+              await miro.board.widgets.update({
+                  id: dice.id, 
+                  text: num.toString(), 
+                  style:{stickerBackgroundColor: color
+                  }
+                }) // update sticker
+
+                sleep(50)
+            }
             
             await sleep(5000);
             await miro.board.widgets.deleteById(dice.id) // delete sticker
