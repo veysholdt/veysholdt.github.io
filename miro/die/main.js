0aba70961a6f8afa187b589dddd28efa04887edc
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-19 18:22:55 +0100

    Update main.js

--- miro/die/main.js
+++ miro/die/main.js
@@ -70,33 +70,33 @@ miro.onReady(() => {
                   widgets.push(dice);
               }
 
-              // for (let i = 0; i < 15; i++) 
-              // {
-              //   var color = get_random_color();
+              for (let i = 0; i < 15; i++) 
+              {
+                var color = get_random_color();
                 
-              //   for (let j = 0; j < positions.length; j++) 
-              //   {
-              //     update_widget(widgets[j], positions[j], color);
-              //   }
+                for (let j = 0; j < positions.length; j++) 
+                {
+                  update_widget(widgets[j], positions[j], color);
+                }
 
-              //   sleep(50);
-              // }
+                sleep(30);
+              }
   
-              // for (let i = 0; i < positions.length; i++) 
-              // {
-              //   update_widget(widgets[i], positions[i], '#5ee335');
-              // }
+              for (let i = 0; i < positions.length; i++) 
+              {
+                update_widget(widgets[i], positions[i], '#5ee335');
+              }
               
-              await sleep(1000);
+              await sleep(4000);
 
               for (let i = 0; i < positions.length; i++) 
               {
-                await miro.board.widgets.deleteById(widgets[i]) // delete sticker #5ee335'
+                await miro.board.widgets.deleteById(widgets[i].id) // delete sticker #5ee335'
               }
             }
             else
             {
-              miro.showNotification('Please select at least one widget as location.')
+              miro.showNotification('Please select at least one existing widget as location.')
             }
 
            
