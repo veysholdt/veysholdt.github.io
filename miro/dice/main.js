a96280b8e90869fd7c472da0ba042f4ece3345e5
veysholdt <63361250+veysholdt@users.noreply.github.com>
2020-12-21 16:54:10 +0100

    added sidebar

--- miro/dice/main.js
+++ miro/dice/main.js
@@ -103,7 +103,7 @@ miro.onReady(() => {
             }
             else // if user is not authorized
             {
-              miro.board.ui.openModal('authorize.html');
+              miro.board.ui.openLeftSidebar('authorize.html');
             }
           }
         }
