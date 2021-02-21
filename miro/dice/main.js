3acfab53303c1a9a4a4daaf3b8daa069ed6fc8e6
veysholdt <63361250+veysholdt@users.noreply.github.com>
2021-02-21 17:39:57 +0100

    Update main.js

--- miro/dice/main.js
+++ miro/dice/main.js
@@ -43,8 +43,8 @@ miro.onReady(() => {
     miro.initialize({
       extensionPoints: {
         toolbar: {
-          title: 'new dice 1',
-          svgIcon: icon24, //'<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+          title: 'new dice',
+          toolbarSvgIcon: icon24, //'<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
           onClick: async () => {
             
             const auth = await miro.isAuthorized();
