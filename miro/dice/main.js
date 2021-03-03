19abd96d38b46a1a8b0e12a45adaf7fdfdb33974
veysholdt <63361250+veysholdt@users.noreply.github.com>
2021-03-03 14:43:10 +0100

    Update main.js

--- miro/dice/main.js
+++ miro/dice/main.js
@@ -31,6 +31,66 @@ async function update_widget(widget, position, color)
     }); // update sticker
 }
 
+function dice_app()
+{
+  let positions = await miro.board.selection.get();
+  let widgets = [];
+
+  if (positions.length >= 1 && positions.length <= 5) // check if at least one and max. 5 widgets are selected
+  {
+    for (let i = 0; i < positions.length; i++) // create a new sticker for every selected widget 
+    {
+      let dice = (await miro.board.widgets.create({ 
+          type:'sticker', 
+          text: get_randome().toString(),
+          x: positions[i].x,
+          y: positions[i].y,
+          capabilities: {
+          "editable": false
+          },
+          style:{
+            backgroundOpacity: 1,
+          },
+        }))[0];
+        
+        widgets.push(dice.id);
+        await sleep(200);
+    }
+      
+    for (let j = 0; j < positions.length; j++) // update every sticker
+    {
+      let color = get_random_color();
+      update_widget(widgets[j], positions[j], color);
+      await sleep(200);
+    }
+
+    for (let i = 0; i < positions.length; i++) // update every sticker for final result
+    {
+      update_widget(widgets[i], positions[i], '#5ee335');
+      await sleep(200);
+    }
+    
+    await sleep(5000);
+
+    for (let i = 0; i < positions.length; i++) // remove stickers again
+    {
+      await miro.board.widgets.deleteById(widgets[i]) // delete sticker
+      await sleep(200);
+    }
+  }
+  else
+  {
+    if(positions.length < 1)
+    {
+      miro.showNotification('Please select at least one existing widget.')
+    }
+    if(positions.length > 5)
+    {
+      miro.showNotification('You are selecting to many widgets, please pick 5 or less.')
+    }
+  }
+}
+
 miro.onReady(() => {
   const icon24 =
     '<g> \
@@ -41,13 +101,13 @@ miro.onReady(() => {
     <ellipse fill="#fff" stroke="#000" stroke-width="2" cx="16.86867" cy="16.61655" id="svg_9" rx="1.8453" ry="1.74359"/> \
     </g>'
   const icon48 = 
-  '<g> \
-  <title>Layer 1</title> \
-  <rect fill="#fff" stroke="#000" stroke-width="5" width="44" height="44" id="svg_1" y="2.01506" x="2.0181" rx="7"/> \
-  <ellipse ry="1.74359" rx="1.8453" id="svg_3" cy="14" cx="14" stroke-width="6" stroke="#000" fill="#fff"/> \
-  <ellipse ry="1.74359" rx="1.8453" id="svg_7" cy="24" cx="24" stroke-width="6" stroke="#000" fill="#fff"/> \
-  <ellipse ry="1.74359" rx="1.8453" id="svg_9" cy="34" cx="34" stroke-width="6" stroke="#000" fill="#fff"/> \
-  </g>'
+    '<g> \
+    <title>Layer 1</title> \
+    <rect fill="#fff" stroke="#000" stroke-width="5" width="44" height="44" id="svg_1" y="2.01506" x="2.0181" rx="7"/> \
+    <ellipse ry="1.74359" rx="1.8453" id="svg_3" cy="14" cx="14" stroke-width="6" stroke="#000" fill="#fff"/> \
+    <ellipse ry="1.74359" rx="1.8453" id="svg_7" cy="24" cx="24" stroke-width="6" stroke="#000" fill="#fff"/> \
+    <ellipse ry="1.74359" rx="1.8453" id="svg_9" cy="34" cx="34" stroke-width="6" stroke="#000" fill="#fff"/> \
+    </g>'
    
     miro.initialize({
       extensionPoints: {
@@ -61,62 +121,7 @@ miro.onReady(() => {
 
             if (auth) // if user is authorized
             {
-              let positions = await miro.board.selection.get();
-              let widgets = [];
-
-              if (positions.length >= 1 && positions.length <= 5) // check if at least one and max. 5 widgets are selected
-              {
-                for (let i = 0; i < positions.length; i++) // create a new sticker for every selected widget 
-                {
-                  let dice = (await miro.board.widgets.create({ 
-                      type:'sticker', 
-                      text: get_randome().toString(),
-                      x: positions[i].x,
-                      y: positions[i].y,
-                      capabilities: {
-                      "editable": false
-                      },
-                      style:{
-                        backgroundOpacity: 1,
-                      },
-                    }))[0];
-                    
-                    widgets.push(dice.id);
-                    await sleep(200);
-                }
-                  
-                for (let j = 0; j < positions.length; j++) // update every sticker
-                {
-                  let color = get_random_color();
-                  update_widget(widgets[j], positions[j], color);
-                  await sleep(200);
-                }
-
-                for (let i = 0; i < positions.length; i++) // update every sticker for final result
-                {
-                  update_widget(widgets[i], positions[i], '#5ee335');
-                  await sleep(200);
-                }
-                
-                await sleep(5000);
-
-                for (let i = 0; i < positions.length; i++) // remove stickers again
-                {
-                  await miro.board.widgets.deleteById(widgets[i]) // delete sticker
-                  await sleep(200);
-                }
-              }
-              else
-              {
-                if(positions.length < 1)
-                {
-                  miro.showNotification('Please select at least one existing widget.')
-                }
-                if(positions.length > 5)
-                {
-                  miro.showNotification('You are selecting to many widgets, please pick 5 or less.')
-                }
-              }
+              dice_app();
             }
             else // if user is not authorized
             {
@@ -124,7 +129,7 @@ miro.onReady(() => {
                 response_type: 'token',
                 redirect_uri: 'https://veysholdt.github.io/miro/dice/authorized.html'
                 }).then(() => {
-                // run the app once again
+                  dice_app(); // run the app once again
               });
             
             }
